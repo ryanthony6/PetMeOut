df971e5d4da31e4dc80912d1a37e57927c7f4efc
CFChristianF <christianfernando671@gmail.com>
2024-03-04 01:10:12 +0700

    Javascript for FAQ

--- script/script.js
+++ script/script.js
@@ -75,3 +75,34 @@ navLinkFAQs.forEach((navLinkFAQ) => {
     navLinkFAQ.classList.add("active");
   });
 });
+
+const adoptionButton = document.getElementById('adoption-button');
+const basicInfoButton = document.getElementById('basic-button');
+const HowToHelpButton = document.getElementById('help-button')
+
+const basicInfoContainer = document.querySelector('.FAQ-container:nth-of-type(1)');
+const adoptionContainer = document.querySelector('.FAQ-container:nth-of-type(2)');
+const HowToHelpContainer = document.querySelector('.FAQ-container:nth-of-type(3)');
+
+basicInfoContainer.style.display = 'block';
+adoptionContainer.style.display = 'none';
+HowToHelpContainer.style.display = 'none';
+
+
+basicInfoButton.addEventListener('click', function() {
+  adoptionContainer.style.display = 'none';
+  basicInfoContainer.style.display = 'block';
+  HowToHelpContainer.style.display = 'none';
+});
+
+adoptionButton.addEventListener('click', function() {
+  basicInfoContainer.style.display = 'none';
+  adoptionContainer.style.display = 'block';
+  HowToHelpContainer.style.display = 'none';
+});
+
+HowToHelpButton.addEventListener('click', function() {
+  adoptionContainer.style.display = 'none';
+  basicInfoContainer.style.display = 'none';
+  HowToHelpContainer.style.display = 'block';
+});
