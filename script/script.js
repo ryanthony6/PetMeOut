9f42fc5fcb3d141dbb1b52395479eba793ff11db
ryanthony6 <exoticfire5@gmail.com>
2024-03-04 18:04:51 +0700

    add angularjs (temporary)

--- script/script.js
+++ script/script.js
@@ -36,7 +36,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
 const accordionContent = document.querySelectorAll(".accordion-content");
 accordionContent.forEach((item, index) => {
   let header = item.querySelector("header");
@@ -51,7 +50,7 @@ accordionContent.forEach((item, index) => {
       description.style.height = "0px";
       item.querySelector("i").classList.replace("fa-minus", "fa-plus");
     }
-    removeOpen(index); 
+    removeOpen(index);
   });
 });
 
@@ -68,41 +67,47 @@ function removeOpen(index1) {
   });
 }
 
-
 const navLinkFAQs = document.querySelectorAll(".nav_link");
 navLinkFAQs.forEach((navLinkFAQ) => {
   navLinkFAQ.addEventListener("click", () => {
-    document.querySelector('.active')?.classList.remove('active')
+    document.querySelector(".active")?.classList.remove("active");
     navLinkFAQ.classList.add("active");
   });
 });
 
-const adoptionButton = document.getElementById('adoption-button');
-const basicInfoButton = document.getElementById('basic-button');
-const HowToHelpButton = document.getElementById('help-button')
+const adoptionButton = document.getElementById("adoption-button");
+const basicInfoButton = document.getElementById("basic-button");
+const HowToHelpButton = document.getElementById("help-button");
 
-const basicInfoContainer = document.querySelector('.FAQ-container:nth-of-type(1)');
-const adoptionContainer = document.querySelector('.FAQ-container:nth-of-type(2)');
-const HowToHelpContainer = document.querySelector('.FAQ-container:nth-of-type(3)');
+const basicInfoContainer = document.querySelector(
+  ".FAQ-container:nth-of-type(1)"
+);
+const adoptionContainer = document.querySelector(
+  ".FAQ-container:nth-of-type(2)"
+);
+const HowToHelpContainer = document.querySelector(
+  ".FAQ-container:nth-of-type(3)"
+);
 
-basicInfoContainer.style.display = 'block';
-adoptionContainer.style.display = 'none';
-HowToHelpContainer.style.display = 'none';
+basicInfoContainer.style.display = "block";
+adoptionContainer.style.display = "none";
+HowToHelpContainer.style.display = "none";
 
-basicInfoButton.addEventListener('click', function() {
-  adoptionContainer.style.display = 'none';
-  basicInfoContainer.style.display = 'block';
-  HowToHelpContainer.style.display = 'none';
+basicInfoButton.addEventListener("click", function () {
+  adoptionContainer.style.display = "none";
+  basicInfoContainer.style.display = "block";
+  HowToHelpContainer.style.display = "none";
 });
 
-adoptionButton.addEventListener('click', function() {
-  basicInfoContainer.style.display = 'none';
-  adoptionContainer.style.display = 'block';
-  HowToHelpContainer.style.display = 'none';
+adoptionButton.addEventListener("click", function () {
+  basicInfoContainer.style.display = "none";
+  adoptionContainer.style.display = "block";
+  HowToHelpContainer.style.display = "none";
 });
 
-HowToHelpButton.addEventListener('click', function() {
-  adoptionContainer.style.display = 'none';
-  basicInfoContainer.style.display = 'none';
-  HowToHelpContainer.style.display = 'block';
+HowToHelpButton.addEventListener("click", function () {
+  adoptionContainer.style.display = "none";
+  basicInfoContainer.style.display = "none";
+  HowToHelpContainer.style.display = "block";
 });
+
