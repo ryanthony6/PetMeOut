751d4a3312c8cdf156e8e82e5fd999ac2ccfc95d
ryanthony6 <exoticfire5@gmail.com>
2024-03-26 17:36:56 +0700

    add multer package and database post for creating pet data

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
-
+require("dotenv").config();
 require("./utils/db");
+
 const Pet = require("./models/petData");
+const multer = require("multer");
 
 const app = express();
-
 const port = process.env.PORT || 5000;
-
 const expressLayouts = require("express-ejs-layouts");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
-
 app.use(expressLayouts);
-
 app.use(express.static("public"));
 
-app.listen(port, () => {
-  console.log(`Webserver app listening on http://localhost:${port}/`);
+// Image upload
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+  },
 });
+
+var upload = multer({ storage: storage }).single("image");
 
 app.get("/", (req, res) => {
   res.render("home.ejs", { title: "homepage", layout: "mainlayout.ejs" });
@@ -41,6 +49,38 @@ app.get("/signinup", (req, res) => {
 });
 
 app.get("/add", (req, res) => {
-  res.render("AdmnForm.ejs", { title: "Admin_form", layout: "AdmnFormLayout.ejs" });
-})
+  res.render("addPet.ejs", { title: "add_pet", layout: "detailslayout.ejs" });
+});
 
+// Insert pet data into database
+app.post("/add", upload,async (req, res) => {
+
+  // if (!req.file) {
+  //   return res.status(400).json({ message: 'No file uploaded', type: 'error' });
+  // }
+
+  const pet = new Pet({
+    name: req.body.name,
+    age: req.body.age,
+    gender: req.body.gender,
+    size: req.body.size,
+    breed: req.body.breed,
+    location: req.body.location,
+    category: req.body.category,
+    image: req.file.filename,
+  });
+
+
+  try {
+    const newPet = await pet.save();
+    res.send("Pet added successfully!");
+  } catch (error) {
+    console.error("Error adding pet:", error.message);
+    res.status(500).send("Error adding pet data");
+  }
+
+});
+
+app.listen(port, () => {
+  console.log(`Webserver app listening on http://localhost:${port}/`);
+});
